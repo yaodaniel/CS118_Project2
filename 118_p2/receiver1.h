8e4d6ab929cc391b212b7335ae7fa6a221f26a4e
qloria <gloriaxch@gmail.com>
2015-11-30 21:12:03 -0800

    packetloss and corruption included

---
+++ 118_p2/receiver1.h
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>      // define structures like hostent
+#include <stdlib.h>
+#include <strings.h>
+#include <packet.h>
+#include <fcntl.h>
+#include <cstring>
+#include <time.h>
+/*GO BACK-N 
+** Receiver **
+1. Doesn't need to keep track of a window
+2. Receiver drops all packets not received in sequence
+3. ACKS server of the correctly in-order received SEQ #
+4. Receiver checks for current SEQ # first before checking last packet flag
+** Sender **
+1. Sequence # starts at 1
+2. Need at least CWND + 1 SEQ #s
+3. 1 timer for entire CWND
+4. Sends all packets in CWND.
+5. When an appropriate ACK is received, increment window
+*/
+double random_prob() {
+	return (rand() % 100)/100.0;
+}
+int main(int argc, char *argv[]) {
+	int receiver_sockfd; //Socket descriptor
+	int portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server; //Contains info regarding server IP and others
+	struct timeval timeout={2,0}; //set timeout for 2 seconds
+	packet* Packet;
+	char* buffer = (char *)calloc(PACKET_SIZE, sizeof(char));
+	unsigned long currentExpectedSeqNumber = 4;
+	double lossProbability = 0.0, corruptionProbability = 0.0; //Assume no loss/corruption by default
+	if(argc != 4 && argc != 6) {
+		fprintf(stderr,"Invalid number of arguments");
+		exit(0);
+	}
+	if(argc == 4) {
+	}
+	if(argc == 6) {
+		lossProbability = atof(argv[4]);
+		corruptionProbability = atof(argv[5]);
+	}
+	portno = atoi(argv[2]);
+	receiver_sockfd = socket(AF_INET, SOCK_DGRAM, 0); //create a new socket
+	if(receiver_sockfd < 0)
+		printf("ERROR opening socket");
+	server = gethostbyname(argv[1]);
+	if(server == NULL) {
+		fprintf(stderr, "ERROR no such host\n");
+		exit(0);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET; //initialize server's address
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(portno);
+	//We use the packet struct to modify buffer's values
+	Packet = (packet*)buffer;
+	//setsockopt(receiver_sockfd, SOL_SOCKET, SO_RCVTIMEO,(char*)&timeout,sizeof(struct timeval));
+	//fcntl(receiver_sockfd, F_SETFL, O_NONBLOCK);
+	socklen_t serv_addr_size = sizeof(serv_addr);
+	//SIMPLIFIED THREE WAY HANDSHAKE
+	while(1) { //initial request
+		//STEP 1
+			createPacket(true, SYN, 1, 2, 0, (char *)"", Packet);
+			n = sendto(receiver_sockfd, buffer, PACKET_SIZE, 0, (const sockaddr*)&serv_addr, serv_addr_size);	
+			if(n < 0)
+				printf("ERROR in sendto\n");
+			else
+				printf("Sent SYN: %d bytes\n", n);
+		while(1) {
+			//NO RESPONSE IN # TIME, BREAK
+			n = recvfrom(receiver_sockfd, buffer, PACKET_SIZE, 0, (sockaddr*) &serv_addr, &serv_addr_size);
+			if(n != -1)
+				break;
+		}
+		//1st packet response received (Should be server's ACK)
+		printf("Received the following packet\n");
+		printPacket(Packet);
+		//STEP 3
+		if(Packet->packetType == SYN_ACK) { //strcmp(Packet->data, "SYN ACK") == 0) {
+			printf("Received server's SYN-ACK\n");
+			createPacket(true, REQUEST, Packet->ACK_num, Packet->ACK_num+1, strlen(argv[3]), argv[3], Packet);
+			n = sendto(receiver_sockfd, buffer, PACKET_SIZE, 0, (const sockaddr*)&serv_addr, serv_addr_size);
+			if(n < 0) {
+				printf("ERROR sending file request, Retrying...\n");
+				printf("Strlen: %d\n", strlen(argv[3]));
+				exit(0);
+			}
+			else {
+				printf("Sent file request: %d bytes\n", n);
+				break;
+			}
+		}
+	}
+	srand(time(NULL));
+	//GO BACK-N FILE TRANSFER STARTS
+	listen: while(1) {
+		//while(1) { //Sender side will time out eventually so recvfrom shouldn't get stuck
+		n = recvfrom(receiver_sockfd, buffer, PACKET_SIZE, 0, (sockaddr*) &serv_addr, &serv_addr_size);
+		//	if(n != -1)
+		//		break;
+		//}
+		if(n < 0 || random_prob() < lossProbability) {//Go back to listen, let server side timeout
+			printf("Packet was lost.\n");
+			goto listen;
+		}
+		else if (random_prob() < corruptionProbability) {
+			printf("Packet corrupted.\n");
+			goto listen;
+		}
+		else if(n >= 0) {
+			printPacket(Packet);
+			printf("received: %zd bytes\n", n);
+			printf("currentExpectedNum: %lu\n", currentExpectedSeqNumber);
+			if(Packet->seq_num == currentExpectedSeqNumber) { //Received the packet we're expecting
+				FILE *f = fopen("receivedFile.txt", "a"); //Open file for appending
+				if (f == NULL) {
+				    printf("Error writing to file! Exiting...\n");
+					//TODO DC w/ server
+				    exit(1);
+				}
+				printf("Writing packet to file...\n");
+				//printf("Packet Data %s\n", ((packet *) buffer)->data);
+				//printf("Test print:%.*s\n", n, Packet->data);
+				//fprintf(f, "%s", Packet->data);
+				printf("Data length: %d\n", strlen(Packet->data));
+				fwrite(Packet->data, sizeof(char), strlen(Packet->data), f);
+				fclose(f);
+				printf("Packet written!\n");
+				if(Packet->isFinalPacket) {
+					printf("Everything Received!...\n");
+					//TODO DO FIN stuff here
+					free(buffer);
+					createPacket(true, FIN, Packet->ACK_num, Packet->ACK_num+1, 0, (char *)"", Packet);
+					n = sendto(receiver_sockfd, buffer, PACKET_SIZE, 0, (const sockaddr*)&serv_addr, serv_addr_size);
+					if(n < 0) {
+						printf("ERROR sending ACK\n");
+					}
+					else {
+						printf("FIN Sent: %d bytes\n", n);
+						break;
+					}
+				}
+				else { //more packets to receive: ACK current received packet.
+					createPacket(true, ACK, Packet->ACK_num, Packet->ACK_num+1, 0, (char *)"", Packet);
+					n = sendto(receiver_sockfd, buffer, PACKET_SIZE, 0, (const sockaddr*)&serv_addr, serv_addr_size);
+					if(n < 0)
+						printf("ERROR sending ACK\n");
+					else {
+						printf("Sent: %d bytes\n", n);
+						currentExpectedSeqNumber = Packet->seq_num+1;
+						printf("currentExpectedSeqNumber: %lu\n", currentExpectedSeqNumber);
+						//if(currentExpectedSeqNumber == 6)
+						//	exit(0);
+					}
+				}
+			}
+			else {
+				printf("Wrong packet, dropping packet number %lu\n", Packet->seq_num);
+				goto listen;
+			}
+			/*else {//Wrong seq number, do nothing let server timeout
+				//Received Packet should be ignored because it is out of ordered/lost/corrupted
+				//Continue listening for other packets
+				createPacket(true, ACK, currentExpectedSeqNumber-1, currentExpectedSeqNumber, 0, (char *)"", Packet);
+				n = sendto(receiver_sockfd, buffer, PACKET_SIZE, 0, (const sockaddr*)&serv_addr, serv_addr_size);
+				if(n < 0)
+					printf("ERROR sending ACK\n");
+				else {
+					printf("Sent: %d bytes\n", n);
+					currentExpectedSeqNumber;
+				}
+				continue;
+			}*/
+		}
+		//Assume program doesn't need to detect if initial file request was lost (just rerun client if no response)
+		//Thus no need to resend initial file req or ACKS (lost ack = server timeout and resend)
+		/*else { //Resend current packet (either initial file request or ACKs)
+			n = sendto(receiver_sockfd, buffer, PACKET_SIZE, 0, (const sockaddr*)&serv_addr, serv_addr_size);	
+			if(n < 0)
+				printf("ERROR in sendto\n");
+			else
+				printf("Sent: %d bytes\n", n);
+		}*/
+	}
+}
---
+++ 118_p2/sender1.h
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <packet.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <fcntl.h>
+#include <time.h>
+#include <iostream>
+
+
+/*GO BACK-N 
+** Receiver **
+1. Doesn't need to keep track of a window
+2. Receiver drops all packets not received in sequence
+3. ACKS server of the correctly in-order received SEQ #
+4. Receiver checks for current SEQ # first before checking last packet flag
+
+** Sender **
+1. Need at least CWND + 1 SEQ #s
+2. 1 timer for entire CWND
+3. Sends all packets in CWND.
+4. When an appropriate ACK is received, increment window
+*/
+#define TIMEOUT 2
+
+int main(int argc, char *argv[]) { //portNumber, CWND, Pr(loss), Pr(corruption)
+	int server_sockfd, newsockfd, portno, window_size = 1, window_start_index = 0; //Default window Size
+	double lossProbability = 0.0, corruptionProbability = 0.0; //Assume no loss/corruption by default
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+	char buffer[PACKET_SIZE];
+	bzero(buffer, PACKET_SIZE);
+	struct timeval timeout={2,0}; //set timeout for 2 seconds
+	packet* Packet = (packet*)buffer;
+	time_t timer;
+
+	if(argc != 2 && argc != 5) {
+		fprintf(stderr, "ERROR, Invalid number of arguments\n");
+		exit(1);
+	}
+	if(argc == 2) { //Only provided a port # assume no loss/corruption
+	}
+	if(argc == 5) { //Use selected values for CWND, loss, & corruption
+		window_size = atoi(argv[2]);
+		lossProbability = atof(argv[3]);
+		corruptionProbability = atof(argv[4]);
+	}
+	
+	//setsockopt(server_sockfd, SOL_SOCKET, SO_RCVTIMEO,(char*)&timeout,sizeof(struct timeval));
+	
+
+
+	portno = atoi(argv[1]);
+	printf("SERVER STARTED ON PORT: %d\n", portno);
+	server_sockfd = socket(AF_INET, SOCK_DGRAM, 0); //Create UDP socket
+		if(server_sockfd < 0)
+			fprintf(stderr, "ERROR creating UDP socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+		if (bind(server_sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+			printf("ERROR on binding");
+	clilen = sizeof(cli_addr);
+	fcntl(server_sockfd, F_SETFL, O_NONBLOCK);
+
+	LISTEN:
+	while(1) {
+		while (1) {
+		ssize_t n = recvfrom(server_sockfd, buffer, PACKET_SIZE, 0, (struct sockaddr *) &cli_addr, &clilen);
+		//if(blah < 0)
+			//printf("ERROR Receiving initial msg%d", blah);
+		if (n > 0)
+			break;
+		}
+
+		printf("Received new connection\n");
+		//Print the packet we received
+		printPacket(Packet);
+		if(Packet->packetType == SYN) {
+			createPacket(true, SYN_ACK, Packet->ACK_num, Packet->ACK_num+1, 0, (char *)"", Packet);
+			int n = sendto(server_sockfd, buffer, PACKET_SIZE, 0, (const sockaddr*)&cli_addr, sizeof(cli_addr));
+				if(n < 0) {
+					printf("ERROR sending SYN ACK\nRetrying...\n");
+				}
+				else
+					printf("Sent SYN ACK: %d bytes\n", n);
+		}
+		else { //We received file request
+			//Print the packet we received
+			printf("Received file request\n");
+			printPacket(Packet);
+		}
+		//Initial packet has a filename in Data section
+		//Search for file in file system
+		printf("Searching for requested file...\n");
+		FILE* fp = fopen((const char*)Packet->data, "r");
+		if(fp == NULL) {
+			fprintf(stderr, "Failed to open requested file\n");
+			//TODO notify client or just ignore?
+			goto LISTEN; //Go back to listening state
+		}
+
+		//File Found, determine how many bytes it is
+		fseek(fp, 0, SEEK_END);
+		long fileSize = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		//printf("DEBUG: FileSize: %lu\n",fileSize);
+
+		//Read file into memory (Not the best idea, but this is CS118)
+		//Calculate number of packets needed
+		unsigned long num_required_packets = std::max(1,(int)((fileSize/DATA_SIZE)+1));
+		printf("DEBUG: Num_Required_Packets: %lu\n", num_required_packets);
+		socklen_t serv_addr_size = sizeof(serv_addr);
+		packet* sendQueue[num_required_packets];
+		char* packetBuffers[num_required_packets];
+		char* datas[num_required_packets];
+
+		//Initialize every packet
+		unsigned long tempSeqNum = Packet->seq_num;
+		unsigned long tempACKNum = Packet->ACK_num;
+		for(int index = 0; index < num_required_packets; index++) {
+			packetBuffers[index] = (char *)calloc(PACKET_SIZE, sizeof(char)); //Dynamic allocated memory here-freed in fin section
+			sendQueue[index] = (packet *)packetBuffers[index];
+			int read = pread(fileno(fp), sendQueue[index]->data, (size_t)DATA_SIZE-nullByte, index*(DATA_SIZE-nullByte));
+			sendQueue[index]->data[read+nullByte] = '\0';
+			//printf("DEBUG: Bytes read from file %d\n", read);
+			if(index == num_required_packets-1) //Last packet, we want to set lastPacket flag to true
+				initializePackets(1, DATA, tempACKNum, tempACKNum+1, read, sendQueue[index]);
+			else
+				initializePackets(0, DATA, tempACKNum, tempACKNum+1, read, sendQueue[index]);
+			tempSeqNum+=2;
+			tempACKNum+=2;
+		}
+
+		//At this point, we have the entire to-be-sent packets in queue.
+		unsigned long receivedUpTo = 3;
+		long sentUpTo = -1; //To track which packets in the window should be sent
+		bool timedOut = false; //To track if we need to resend all packets in window
+
+		while(window_start_index < num_required_packets) {
+			//Send every packet in our window that has yet to be sent; jump here upon timeouts
+			resend_window:
+			for(int index = window_start_index; index < window_start_index + window_size; index++) {
+				time(&timer); //Set/reset timer for this window	
+				//Send packets in window that haven't been sent, or if timeout, resend all packets in window
+				if (index > sentUpTo || timedOut == true) {
+					int n = sendto(server_sockfd, packetBuffers[index], PACKET_SIZE, 0, (const sockaddr*)&cli_addr, sizeof(cli_addr));
+					if(n < 0)
+						printf("ERROR sending packet #: %d\n", index);
+					else {
+						printf("Sent: %d bytes\n", n);
+						sentUpTo = index;				
+					}
+				}
+			}
+			if (timedOut == true) //resent window, reset timeout flag
+				timedOut = false;
+			//Wait to receive an ACK, or a timeout
+			while(1) {
+				//TODO IF NO RESPONSE IN # TIME, BREAK
+				//printf("Waiting to receive ACK");
+				//printf("time now: %ld, timer+timeout: %ld", time(NULL), timer+TIMEOUT);
+				if (time(NULL) > timer + TIMEOUT && timedOut == false) {
+					//std::cout << "timing out\n";
+					printf("Timing out, resending window from packet num %d", window_start_index);
+					timedOut = true;
+					goto resend_window;
+				}
+				if (recvfrom(server_sockfd, buffer, PACKET_SIZE, 0, (struct sockaddr *) &cli_addr, &clilen) > 0)
+					break;
+			}
+			//Print the packet we received
+			printPacket(Packet);
+			if(Packet->packetType == FIN) {
+				printf("FIN received... Transfer complete! :)\n\n");
+				//TODO send FIN-ACK back?
+				//go back to initial loop and listen for new requests
+				printf("AWAITING REQUESTS ON PORT: %d\n", portno);			
+				bzero(buffer, PACKET_SIZE);
+				window_start_index = 0;
+				for (int i = 0; i < num_required_packets; i++) { //Free calloc'ed memory
+					free(packetBuffers[i]);
+				}
+				fclose(fp);
+				goto LISTEN;
+			}
+			//Check if we need to move window start
+			if((Packet->seq_num > receivedUpTo)) {
+				window_start_index += (Packet->seq_num - receivedUpTo)/2;
+				receivedUpTo = Packet->seq_num;
+				//TODO if we move window start, refresh timer
+				//timer is refreshed at the sending of first packet in window in next loop
+				//time(&timer);
+			}
+			else {
+				//TODO, do nothing?... maybe timer related things will go here
+			}
+		}
+	}
+}
